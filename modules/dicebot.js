7c3c158a095c0a3f8b814c5c76dd15b7ae725820
Tony Dracéus <44443117+TonyDrac@users.noreply.github.com>
2020-03-04 13:08:02 +0100

    More replies
    Added array for random reply messages

--- modules/dicebot.js
+++ modules/dicebot.js
@@ -2,28 +2,40 @@ const config = require('../config.js');
 const axios = require('axios')
 const sleep = require('util').promisify(setTimeout)
 
+var replies = [
+    'Hokus Pokus.. ',
+    'Simsalabim! ',
+    'Du fåååår.. ',
+    'Ojojoj! ',
+    '🎲 ',
+    'Jag visste att du ville ha ',
+    'Tärningen är kastad.. ',
+    'Åh, mitt lyckonummer ',
+    'Var det den här siffran du ville ha? '
+]
+
 module.exports = {
-    init: function(client){
-        client.on('message',(msg) => {
-          if(msg.content.length <= 20){
-            roll(msg).then(console.log).catch(console.err);
-          }
+    init: function (client) {
+        client.on('message', (msg) => {
+            if (msg.content.length <= 20) {
+                roll(msg).then(console.log).catch(console.err);
+            }
         })
     }
 }
 
-
-async function roll(msg){
-  try{
-    const response = await axios.get(`https://${config.dicebot.url}:${config.dicebot.port}/roll/${msg.content}`);
-    if(response.data.result && !response.data.trivial){
-      let reply = await msg.reply(`🎲${msg.content}🎲`);
-      await sleep(200);
-      await reply.edit(`🎲 ${response.data.result} 🎲`)
+async function roll(msg) {
+    try {
+        const response = await axios.get(`https://${config.dicebot.url}:${config.dicebot.port}/roll/${msg.content}`);
+        if (response.data.result && !response.data.trivial) {
+            let reply = await msg.reply(`🎲${msg.content}🎲`);
+            await sleep(200);
+            var randomNumber = Math.floor(Math.random() * (replies.length));
+            await reply.edit(replies[randomNumber] + `${response.data.result}! 🎲`)
+        }
+    } catch (error) {
+        if (error.response && error.response.status != 404) {
+            console.error(error);
+        }
     }
-}catch(error){
-    if(error.response && error.response.status != 404){
-          console.error(error);
-    }
-  }
 }
